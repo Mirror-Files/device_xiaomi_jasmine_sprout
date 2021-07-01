acd26b2d913739b214538b41212f85772f200c57
Paras Bansal <55031121+ParasBansal25@users.noreply.github.com>
2021-07-01 20:50:08 +0530

    Update xtended_jasmine_sprout.mk

--- xtended_jasmine_sprout.mk
+++ xtended_jasmine_sprout.mk
@@ -19,7 +19,6 @@ $(call inherit-product, device/xiaomi/jasmine_sprout/device.mk)
 
 # Inherit BlissROM product configuration
 $(call inherit-product, vendor/xtended/config/common_full_phone.mk)
-$(call inherit-product, vendor/gapps/core/config.mk)
 
 # Define first api level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
