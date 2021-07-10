c37fd5e3a8df821537e2647d127b86543de9c8f0
Mirror-Files <84989006+Mirror-Files@users.noreply.github.com>
2021-07-10 21:02:08 +0530

    Update xtended_jasmine_sprout.mk

--- xtended_jasmine_sprout.mk
+++ xtended_jasmine_sprout.mk
@@ -19,6 +19,9 @@ $(call inherit-product, device/xiaomi/jasmine_sprout/device.mk)
 
 # Inherit XtendedROM product configuration
 $(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+$(call inherit-product, vendor/gapps/basic/config.mk)
+$(call inherit-product, vendor/gapps/core/config.mk)
+
 
 # Define first api level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
