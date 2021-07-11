21648feb983d8fb464ce4a04a8ccf11ba6d8cd32
Mirror-Files <84989006+Mirror-Files@users.noreply.github.com>
2021-07-11 20:26:24 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -17,9 +17,9 @@
 
 # inherit MI A2 device config for BlissROM
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/pa_jasmine_sprout.mk
+    $(LOCAL_DIR)/p404_jasmine_sprout.mk
 
 COMMON_LUNCH_CHOICES := \
-    pa_jasmine_sprout-user \
-    pa_jasmine_sprout-userdebug \
-    pa_jasmine_sprout-eng
+    p404_jasmine_sprout-user \
+    p404_jasmine_sprout-userdebug \
+    p404_jasmine_sprout-eng
