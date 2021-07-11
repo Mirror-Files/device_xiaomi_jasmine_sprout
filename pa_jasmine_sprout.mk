698d979b6cc79a7d1c9f7332f08471f753e7f4a2
Mirror-Files <84989006+Mirror-Files@users.noreply.github.com>
2021-07-11 19:27:54 +0530

    Update pa_jasmine_sprout.mk

--- pa_jasmine_sprout.mk
+++ pa_jasmine_sprout.mk
@@ -18,7 +18,7 @@
 $(call inherit-product, device/xiaomi/jasmine_sprout/device.mk)
 
 # Inherit BlissROM product configuration
-$(call inherit-product, vendor/404/configs/common.mk)
+$(call inherit-product, vendor/pa/config/common_full_phone.mk)
 
 # Define first api level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
