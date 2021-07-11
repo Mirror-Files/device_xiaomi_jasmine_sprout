6f2d50e35d6faa7b070aaa8504ad772045629b00
Mirror-Files <hyderabad0000001@gmail.com>
2021-07-11 15:39:19 +0530

    new

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -17,9 +17,9 @@
 
 # inherit MI A2 device config for BlissROM
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/syberia_jasmine_sprout.mk
+    $(LOCAL_DIR)/p404_jasmine_sprout.mk
 
 COMMON_LUNCH_CHOICES := \
-    syberia_jasmine_sprout-user \
-    syberia_jasmine_sprout-userdebug \
-    syberia_jasmine_sprout-eng
+    p404_jasmine_sprout-user \
+    p404_jasmine_sprout-userdebug \
+    p404_jasmine_sprout-eng
--- p404_jasmine_sprout.mk
+++ p404_jasmine_sprout.mk
@@ -18,7 +18,7 @@
 $(call inherit-product, device/xiaomi/jasmine_sprout/device.mk)
 
 # Inherit BlissROM product configuration
-$(call inherit-product, vendor/syberia/config/common.mk)
+$(call inherit-product, vendor/404/config/common.mk)
 
 # Define first api level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
@@ -37,7 +37,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 # Device identifier
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := syberia_jasmine_sprout
+PRODUCT_NAME := p404_jasmine_sprout
 PRODUCT_DEVICE := jasmine_sprout
 PRODUCT_MODEL := Mi A2
 
@@ -56,5 +56,3 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # TWRP Support
 BUILD_TWRP := true
-
-SYBERIA_BUILD_TYPE := UNOFFICIAL
