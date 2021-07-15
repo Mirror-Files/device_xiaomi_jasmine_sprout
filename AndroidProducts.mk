a899e6937a4faf94e00acf2e3e43e3cb86290ad9
Mirror-Files <84989006+Mirror-Files@users.noreply.github.com>
2021-07-15 13:17:54 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -17,9 +17,9 @@
 
 # inherit MI A2 device config for BlissROM
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/xperience_jasmine_sprout.mk
+    $(LOCAL_DIR)/aosp_jasmine_sprout.mk
 
 COMMON_LUNCH_CHOICES := \
-    xperience_jasmine_sprout-user \
-    xperience_jasmine_sprout-userdebug \
-    xperience_jasmine_sprout-eng
+    aosp_jasmine_sprout-user \
+    aosp_jasmine_sprout-userdebug \
+    aosp_jasmine_sprout-eng
