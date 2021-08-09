8a236e7f427d9a88eebf7d0c035e4565e87c5a0f
Mirror-Files <84989006+Mirror-Files@users.noreply.github.com>
2021-08-09 11:45:33 +0530

    Update and rename xdroid_jasmine_sprout.mk to p404_jasmine_sprout.mk

--- p404_jasmine_sprout.mk
+++ p404_jasmine_sprout.mk
@@ -18,7 +18,7 @@
 $(call inherit-product, device/xiaomi/jasmine_sprout/device.mk)
 
 # Inherit BlissROM product configuration
-$(call inherit-product, vendor/xdroid/config/common.mk)
+$(call inherit-product, vendor/404/configs/common.mk)
 
 # Define first api level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
@@ -37,7 +37,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 # Device identifier
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := xdroid_jasmine_sprout
+PRODUCT_NAME := p404_jasmine_sprout
 PRODUCT_DEVICE := jasmine_sprout
 PRODUCT_MODEL := Mi A2
 
